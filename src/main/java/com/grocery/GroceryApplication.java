74bbcffc8f7e861ee00517e1cc07de2b482a76f6
Jerik-Brahmos <jerikbrahmos2004@gmail.com>
2025-02-26 20:43:19 +0530

    Removed secret from history

--- src/main/java/com/grocery/GroceryApplication.java
+++ src/main/java/com/grocery/GroceryApplication.java
@@ -6,20 +6,17 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableCaching
 public class GroceryApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(GroceryApplication.class, args);
 	}
+
 	@Bean
 	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> containerCustomizer() {
 		return factory -> factory.setPort(8080);
 	}
-
 }
